401f296a044cfa7812e5d8fc6fb7950ce5668685
Natsumi <retrogreensnake@gmail.com>
2020-01-08 20:44:19 +0200

    implement it in the disassembly now

--- build.bat
+++ build.bat
@@ -1,16 +1,16 @@
 @echo off
-"AMPS\Includer.exe" ASM68K driver driver\.Data
+"AMPS\Includer.exe" ASM68K AMPS AMPS\.Data
 asm68k /m /p /o ae- sonic1.asm, s1built.dat, , .lst>.log
 type .log
 if NOT EXIST s1built.dat pause & exit
-call driver/z80.bat
+call AMPS/z80.bat
 error\convsym .lst s1built.md -input asm68k_lst -inopt "/localSign=. /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
 fixheadr.exe s1built.md
-del driver\.Data
-del driver\z80.bat
-del driver\merge.asm
-del driver\fix.asm
-del driver\.z80
-del driver\._z80
-del driver\.z80.kos
+del AMPS\.Data
+del AMPS\z80.bat
+del AMPS\merge.asm
+del AMPS\fix.asm
+del AMPS\.z80
+del AMPS\._z80
+del AMPS\.z80.kos
 del s1built.dat
